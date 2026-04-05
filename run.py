49160912bc1a3abee1997f0f0605e7538972c927
dda915 <dda915@gmail.com>
2026-04-05 19:00:19 +0900

    Remove reload for production

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from backend.seed import seed
 if __name__ == "__main__":
     seed()
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
